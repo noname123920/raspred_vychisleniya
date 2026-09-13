efdee1e08c31ad0e205725d38a11cf25d58994b0
Kitkuk <110992990+noname123920@users.noreply.github.com>
2026-09-13 17:43:02 +0500

    /

--- lab1/zad1.cpp
+++ lab1/zad1.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
 #include <omp.h>
+#include <cstdio>
 
 int main(){
-#pragma omp parallel num_threads(4)
-{
-int id = omp_get_thread_num();
+    #pragma omp parallel num_threads(4)
+    {
+        int id = omp_get_thread_num();
 
-if (id % 2 == 0) {
-std::cout << "Even-numbered thread: " << id << "\n";
-}
-else {
-std::cout << "Odd-numbered thread: " << id << "\n";
-}
-}
+        if (id % 2 == 0) {
+            printf("\nEven-numbered thread: %d", id);
+        }
+        else {
+            printf("\nOdd-numbered thread: %d", id);
+        }
+    }
 
-return 0;
+    return 0;
 }
